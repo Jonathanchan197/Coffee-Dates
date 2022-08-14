7f35d3d93111388dd3dd3518466877469dc214fc
Nelson Feng <nelsonfeng98@gmail.com>
2022-08-14 17:00:04 +1000

    added sign in functionality

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useState} from 'react';
+import {useAuth} from '../auth';
 
-const SignIn = () => {
+const Signin = () => {
+    const auth = useAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSignIn = async (e) => {
+        e.preventDefault();
+        const signIn =  await auth.login(email, password);
+
+        if(signIn.error){
+            setMessage(signIn.error.message)
+        } else {
+            setMessage("Successfully Logged in");
+        } 
+
+        setPassword("");
+    }
+
     return (
         <div>
-            <h1>This is the SignIn page</h1>
+            {message && message}
+        <h1>This is Signin</h1>
+
+        <form onSubmit={handleSignIn}>
+            <input type="email" value={email} onChange={ e => setEmail(e.target.value)} />
+            <input type="password" value={password} onChange={ e => setPassword(e.target.value)} />
+            <button type={'submit'}>Login</button>
+        </form>
         </div>
     );
-}
+};
 
-export default SignIn;
+export default Signin;
