4fdb845bdadf3126c8383aacd928b15f39e866d9
Nelson Feng <nelsonfeng98@gmail.com>
2022-08-14 16:38:19 +1000

    Started Sign up Functionality

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import "./index.css";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import { AuthProvider } from "./auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path={"sign-up"} element={<SignUp />} />
-        <Route path={"sign-in"} element={<SignIn />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path={"sign-up"} element={<SignUp />} />
+          <Route path={"sign-in"} element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, {useState} from 'react';
+import {useAuth} from '../auth'
 
 const SignUp = () => {
+    const auth = useAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const register =  await auth.register(email, password);
+
+        if(register.error){
+            setMessage(register.error.message)
+        } else {
+            setMessage("Sucessfully Signed up!")
+        } 
+
+        setEmail("");
+        setPassword("");
+    }
+
     return (
         <div>
-            <h1>This is the SignUp page</h1>
+            {message && message}
+        <h1>This is register</h1>
+
+        <form onSubmit={handleRegister}>
+            <input type="email" value={email} onChange={ e => setEmail(e.target.value)} />
+            <input type="password" value={password} onChange={ e => setPassword(e.target.value)} />
+            <button type={'submit'}>Sign Up</button>
+        </form>
+
         </div>
     );
-}
+};
 
 export default SignUp;
