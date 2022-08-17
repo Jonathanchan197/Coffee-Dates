67d696b286c89f34bc5508b4b7f1c9bb79d4ab20
Jacqueline Chamnankool <jchamnankool@gmail.com>
2022-08-17 23:10:21 +1000

    Got specific mentor upon swipe

--- src/pages/Match.js
+++ src/pages/Match.js
@@ -1,47 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { supabase } from "../supabase";
 import TinderCard from "react-tinder-card";
-import './TinderCard.css';
+import "./TinderCard.css";
 
 const Match = () => {
-    const [people, setPeople] = useState([]);
+  const [people, setPeople] = useState([]);
 
-    const onSwipe = (direction) => {
-        console.log('You swiped: ' + direction)
-    }
+  const onSwipe = (direction) => {
+    console.log(`You swiped ${direction}.`);
+  };
 
-    useEffect(() => {
-        const fetchMentor = async () => {
-          const {data} = await supabase
-            .from('mentors').select('*')
-            setPeople(data)
-            if (!data) {
-                alert('no mentors found!')
-                return
-            }
-        };
-        fetchMentor();
-      }, []);
+  const onCardLeftScreen = (id) => {
+    console.log(id + " left the screen");
+  };
+
+  useEffect(() => {
+    const fetchMentor = async () => {
+      const { data } = await supabase.from("mentors").select("*");
+      setPeople(data);
+      if (!data) {
+        alert("no mentors found!");
+        return;
+      }
+    };
+    fetchMentor();
+  }, []);
+
   return (
     <div>
       <h1>This is the Match page</h1>
-       <div className="card_container">
-      {people.map(person => (
-        <TinderCard
+      <div className="card_container">
+        {people.map((person) => (
+          <TinderCard
             className="swipe"
             key={person.id}
-            onSwipe={onSwipe} 
-            preventSwipe={['up','down']}
-        >
-            <div 
-            style={{backgroundImage: `url("https://yvjzibmcgvuhvzzulirq.supabase.co/storage/v1/object/public/${person.avatar_url}")`}} 
-            className="card"
+            onSwipe={onSwipe}
+            onCardLeftScreen={() => onCardLeftScreen(person.id)}
+            preventSwipe={["up", "down"]}
+          >
+            
+            <div
+              style={{
+                backgroundImage: `url("https://yvjzibmcgvuhvzzulirq.supabase.co/storage/v1/object/public/${person.avatar_url}")`,
+              }}
+              className="card"
             >
-                <h3>{person.name}</h3>
-                <h4>{person.industry}</h4>  
+              <h3>{person.name}</h3>
+              <h4>{person.industry}</h4>
             </div>
-        </TinderCard>
-      ))} 
+          </TinderCard>
+        ))}
       </div>
     </div>
   );
