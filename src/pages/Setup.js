ce70f7fe60c970d57c8c7d11a1906f9f179a17f5
Nelson Feng <nelsonfeng98@gmail.com>
2022-08-17 11:10:16 +1000

    button issue

--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -2,56 +2,82 @@ import { useState } from "react";
 import First from "../forms/First";
 import Second from "../forms/Second";
 import Third from "../forms/Third";
-import Fourth from "../forms/Fourth"
+import Fourth from "../forms/Fourth";
 import Review from "../forms/Review";
 
 const Setup = () => {
   const [page, setPage] = useState(0);
   const [formData, setFormData] = useState({
     isMentor: null,
-    industry: '',
-    image: '',
-    bio: '',
-    name: '',
-    website: '',
+    industry: "",
+    image: "",
+    bio: "",
+    name: "",
+    website: "",
     skills: [],
   });
 
   const whichPage = () => {
     switch (page) {
       case 0:
-        return <First formData={formData} setFormData={setFormData}/>;
+        return <First formData={formData} setFormData={setFormData} />;
       case 1:
-        return <Second formData={formData} setFormData={setFormData}/>;
+        return <Second formData={formData} setFormData={setFormData} />;
       case 2:
-        return <Third formData={formData} setFormData={setFormData}/>;
+        return <Third formData={formData} setFormData={setFormData} />;
       case 3:
-        return <Fourth formData={formData} setFormData={setFormData}/>;
+        return <Fourth formData={formData} setFormData={setFormData} />;
       case 4:
         return <Review formData={formData} />;
       default:
-        return <First formData={formData} setFormData={setFormData}/>;
+        return <First formData={formData} setFormData={setFormData} />;
     }
   };
 
   const handleBack = () => {
-    if(page < 0){
+    if (page < 0) {
       return 0;
     }
     setPage(page - 1);
-  }
+  };
 
   const handleNext = () => {
     setPage(page + 1);
+  };
+
+  const whichButtons = () => {
+    if (page >= 4) {
+      return buttons = (
+        <button className="logout" onClick={handleBack}>
+          Back
+        </button>
+      );
+    }
+    if (page <= 1) {
+      return buttons = (
+        <button className="logout" onClick={handleNext}>
+          Next
+        </button>
+      );
+    } else {
+      return buttons = (
+        <>
+          <button className="logout" onClick={handleBack}>
+            Back
+          </button>
+          <button className="logout" onClick={handleNext}>
+            Next
+          </button>
+        </>
+      );
   }
+};
 
   return (
     <div>
       <h1>This is the setup page.</h1>
       <div>
-        {whichPage()}
-        <button className="logout" onClick={handleBack} >Back</button>
-        <button className="logout" onClick={handleNext}>Next</button>
+        <whichPage />
       </div>
     </div>
   );
