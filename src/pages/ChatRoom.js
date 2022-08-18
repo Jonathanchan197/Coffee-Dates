ff60bac3675e68f17622e781d8ede16420fd435f
Jacqueline Chamnankool <jchamnankool@gmail.com>
2022-08-19 09:42:36 +1000

    Cleanup

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../supabase";
 import { useParams } from "react-router-dom";
-// import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
   MainContainer,
   ChatContainer,
@@ -15,7 +15,7 @@ const ChatRoom = () => {
   const auth = useAuth();
   const { chatId } = useParams();
   const [messages, setMessages] = useState([]);
-  
+
   const fetchMessages = async () => {
     const {data} = await supabase.from('messages').select().match({room_id: chatId})
     setMessages(data)
