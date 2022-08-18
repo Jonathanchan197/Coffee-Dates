abd81aaa1742ea899ed762e2d995fee7dd289897
Jonathan Chan <Jonathanchan197@gmail.com>
2022-08-18 16:38:08 +1000

    no changes

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../supabase";
+import { AuthUser } from "@supabase/supabase-js";
+import { useParams } from 'react-router-dom'
 
 const ChatRoom = () => {
-  return <div>Chatroom page</div>;
+    const {roomId} = useParams()
+    console.log(roomId)
+
+  return (
+    <div>   
+        Chatroom page
+    </div>
+  )
 };
+
 export default ChatRoom;
